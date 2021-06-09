090fb9719e0c36c54bc69aa5bd3a35423431983c
Jason Sprouse <jason@goodfaith.church>
2021-06-08 21:11:55 -0500

    + Nortary Star test

--- test/TestStarNotary.js
+++ test/TestStarNotary.js
@@ -77,7 +77,12 @@ it('lets user2 buy a star and decreases its balance in ether', async() => {
 
 it('can add the star name and star symbol properly', async() => {
     // 1. create a Star with different tokenId
+    let instance = await StarNotary.deployed();
+    let starId = 6;
+    await instance.createStar('Notary Star 6!!', starId, {from:accounts[0]})
     //2. Call the name and symbol properties in your Smart Contract and compare with the name and symbol provided
+    assert.equal(await instance.name.call(), 'Notary Star');
+    assert.equal(await instance.symbol.call(), 'NST');
 });
 
 it('lets 2 users exchange stars', async() => {
